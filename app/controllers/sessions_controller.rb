0018fab0f6e3e7ed12006e3cc69087293a93e091
Le Anh Viet <leanhviet24495@gmail.com>
2017-11-16 14:48:16 +0700

    Fix Finish chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,20 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_back_or user
     else
-      flash.now[:danger] = t "controllers.sessions.invalid"
-      render :new
+      flash_message
     end
   end
 
+  def flash_message
+    flash.now[:danger] = t "controllers.sessions.invalid"
+    render :new
+  end
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
